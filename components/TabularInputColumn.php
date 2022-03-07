6aef294fcba935dd1763ea589d9226ed0a54425f
Radin Černý <radin.cerny@gmail.com>
2022-03-07 18:32:26 +0100

    Custom grid columns.

--- components/TabularInputColumn.php
+++ components/TabularInputColumn.php
@@ -21,12 +21,7 @@ class TabularInputColumn extends \yii\grid\DataColumn
   /**
    * @var array Some input types require a list of data to be displayed. See for example \yii\helpers\Html::activeCheckboxList()
    */
-  public $listData = [];
-
-  /**
-   * @var string Default text for <select> without a selected value. See \yii\helpers\Html::activeDropDownList()
-   */
-  public $listDataPrompt = 'Select a value ...';
+  public $items = [];
 
   /**
    * @var array Standars options for the input. See for example \yii\helpers\Html::activeDropDownList()
@@ -58,20 +53,20 @@ class TabularInputColumn extends \yii\grid\DataColumn
     'textInput',
   ];
 
-  /**
-   * @var string[] These methods in \yii\helpers\Html require listData
-   */
-  private $allowedHtmlMethodNames_listDataRequired = [
-    'activeCheckboxList',
-    'activeDropDownList',
-    'activeListBox',
-    'activeRadioList',
-
-    'checkboxList',
-    'dropDownList',
-    'listBox',
-    'radioList',
-  ];
+//  /**
+//   * @var string[] These methods in \yii\helpers\Html require items
+//   */
+//  private $allowedHtmlMethodNames_itemsRequired = [
+//    'activeCheckboxList',
+//    'activeDropDownList',
+//    'activeListBox',
+//    'activeRadioList',
+//
+//    'checkboxList',
+//    'dropDownList',
+//    'listBox',
+//    'radioList',
+//  ];
 
 
   public function init()
@@ -82,17 +77,19 @@ class TabularInputColumn extends \yii\grid\DataColumn
       throw new InvalidConfigException('The "activeMethodName" property has invalid value. Check class ' . get_class() . ' for details.');
     }
 
-    if (!is_array($this->listData)) {
-      throw new InvalidConfigException('The "listData" property must be an array.');
+    if (!is_array($this->items)) {
+      throw new InvalidConfigException('The "items" property must be an array.');
     }
 
   }
 
   protected function renderDataCellContent($model, $key, $index)
   {
-    $pkNamePrefix = '';
-    foreach ($model->getPrimaryKey(true) as $pkCol => $pkVal) {
-      $pkNamePrefix .= '[' . $pkVal . ']';
+    if ($this->inputOptions['name'] ?? '' == '') {
+      $this->inputOptions['name'] = '';
+      foreach ($model->getPrimaryKey(true) as $pkCol => $pkVal) {
+        $this->inputOptions['name'] .= '[' . $pkVal . ']';
+      }
     }
 
     $errorText = '';
@@ -105,9 +102,11 @@ class TabularInputColumn extends \yii\grid\DataColumn
 
     $this->inputOptions['class'] = trim(($this->inputOptions['class'] ?? '') . ' form-control ' . $errorClass);
     $this->inputOptions['title'] = $errorText;
-    $this->inputOptions['prompt'] = $this->listDataPrompt;
 
     $htmlMethodName = $this->htmlMethodName;
+    $attribute = $this->attribute;
+    $value = $model->$attribute;
+
     switch ($htmlMethodName) {
       case 'activeCheckbox':
       case 'activeHiddenInput':
@@ -115,7 +114,7 @@ class TabularInputColumn extends \yii\grid\DataColumn
       case 'activeTextarea':
       case 'activeTextInput':
         // activeTextInput($model, $attribute, $options = [])
-        return Html::$htmlMethodName($model, $this->inputNamePrefix . $pkNamePrefix . $this->attribute, $this->inputOptions);
+        return Html::$htmlMethodName($model, $this->inputNamePrefix . $this->inputOptions['name'] . $this->attribute, $this->inputOptions);
 
       case 'checkbox':
       case 'hiddenInput':
@@ -123,21 +122,21 @@ class TabularInputColumn extends \yii\grid\DataColumn
       case 'textarea':
       case 'textInput':
         // textInput($name, $value = null, $options = [])
-        // return Html::$htmlMethodName($model, $this->inputNamePrefix . $pkNamePrefix . $this->attribute, $this->inputOptions);
+        return Html::$htmlMethodName($this->inputNamePrefix . $this->inputOptions['name'] . $this->attribute, $value, $this->inputOptions);
 
       case 'activeCheckboxList':
       case 'activeDropDownList':
       case 'activeListBox':
       case 'activeRadioList':
         // activeCheckboxList($model, $attribute, $items, $options = [])
-        return Html::$htmlMethodName($model, $this->inputNamePrefix . $pkNamePrefix . $this->attribute, $this->listData, $this->inputOptions);
+        return Html::$htmlMethodName($model, $this->inputNamePrefix . $this->inputOptions['name'] . $this->attribute, $this->items, $this->inputOptions);
 
       case 'checkboxList':
       case 'dropDownList':
       case 'listBox':
       case 'radioList':
         // checkboxList($name, $selection = null, $items = [], $options = [])
-        // return Html::$htmlMethodName($model, $this->inputNamePrefix . $pkNamePrefix . $this->attribute, $this->listData, $this->inputOptions);
+        return Html::$htmlMethodName($this->inputNamePrefix . $this->inputOptions['name'] . $this->attribute, $value, $this->items, $this->inputOptions);
     }
 
   }
